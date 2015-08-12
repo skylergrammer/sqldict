9e2e98ff13df42116843b0ba6ff37f9e78797eef
sgrammer <skylergrammer@gmail.com>
2015-08-12 16:43:15 -0500

    modified to take as parameters table_name, key_col, and val_col. Default remain to be kv_store, key, and val

--- sqldict.py
+++ sqldict.py
@@ -9,7 +9,7 @@ def make_sql_table(kv_list, db_name, key_format="String", value_format="BLOB"):
     except Exception, e:
         sys.exit(e)
     c = conn.cursor()
-    c.execute('''CREATE TABLE kv_store (%s String PRIMARY KEY, val  %s)''' %
+    c.execute('''CREATE TABLE kv_store (key %s PRIMARY KEY, val  %s)''' %
               (key_format, value_format))
     try:
         for k,v in kv_list:
@@ -28,17 +28,23 @@ def make_sql_table(kv_list, db_name, key_format="String", value_format="BLOB"):
 
 
 class SqlDict(object):
-    def __init__(self, name, deserialize=True):
+    def __init__(self, name,
+                 table_name="kv_store", key_col="key", val_col="val",
+                 deserialize=True):
         if not name.endswith('.db'):
             name = name + '.db'
         self.name = expanduser(name)
         self.conn = connect(self.name)
         cur = self.conn.cursor()
         self.deserialize = deserialize
+        self.__tablename = table_name
+        self.__key_col = key_col
+        self.__val_col = val_col
 
     def __getitem__(self, key, default=None):
         cur = self.conn.cursor()
-        cur.execute("SELECT val FROM kv_store WHERE key=? LIMIT 1", (key,))
+        cur.execute("SELECT %s FROM %s WHERE %s=? LIMIT 1" %
+                    (self.__val_col, self.__tablename, self.__key_col), (key,))
         res = cur.fetchone()
         try:
             if not self.deserialize:
@@ -53,26 +59,26 @@ class SqlDict(object):
 
     def __contains__(self, key):
         cur = self.conn.cursor()
-        cur.execute("SELECT COUNT(*) FROM kv_store WHERE key=? LIMIT 1", (key,))
+        cur.execute("SELECT COUNT(*) FROM %s WHERE %s=? LIMIT 1" %
+                    (self.__tablename, self.__key_col), (key,))
         res = cur.fetchone()
         return res[0] != 0
 
     def values(self):
         cur = self.conn.cursor()
-        cur.execute("k val FROM kv_store")
+        cur.execute("SELECT %s FROM %s" % (self.__val_col, self.__tablename))
         res = (x[0] for x in cur)
         return res
 
     def keys(self):
         cur = self.conn.cursor()
-        cur.execute("SELECT key FROM kv_store")
+        cur.execute("SELECT %s FROM %s" % (self.__key_col, self.__tablename))
         res = (x[0] for x in cur)
         return res
 
     def items(self):
         cur = self.conn.cursor()
-        cur.execute("SELECT key,val FROM kv_store")
+        cur.execute("SELECT %s,%s FROM %s" %
+                    (self.__key_col, self.__val_col, self.__tablename))
         res = (x for x in cur)
         return res
-
-
